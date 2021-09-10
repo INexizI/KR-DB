cdb2841eb5cb586ed52f5591575efd4b95bda3c2
INexizI <39536903+INexizI@users.noreply.github.com>
2021-09-10 15:11:51 +0300

    rollback to prev state
    *need fix skill page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,3 @@
-// require.context('../images', true)
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -6,13 +5,13 @@ require("channels")
 require("jquery-ui")
 require("packs/char")
 require("packs/gear")
-require("packs/calc")
+// require("packs/calc")
 require("packs/stat")
 require("packs/search")
 require("packs/jquery.easy-autocomplete.js")
 
-import 'src/stylesheets/application'
-const images = require.context('../images', true)
+// import 'src/stylesheets/application'
+// const images = require.context('../images', true)
 
 Rails.start()
 Turbolinks.start()
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # config.action_view.image_loading = "lazy"
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -53,6 +54,9 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  config.assets.compress = true
+  config.assets.js_compressor  = :uglifier
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
