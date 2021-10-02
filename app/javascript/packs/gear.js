70f53f464beb0bca4eb04671e32fc583384d9e20
INexizI <39536903+INexizI@users.noreply.github.com>
2021-10-02 11:50:14 +0300

    upd gear page

--- app/javascript/packs/gear.js
+++ app/javascript/packs/gear.js
@@ -1,12 +1,12 @@
 (function() {
   $(document).on("turbolinks:load", function() {
-    $('.g-title').click(function() {
-      $cssVal = {
-        'display':'flex',
-        'flex-wrap':'wrap'
-      }
-      $(this).next('.g-main').slideToggle('fast').css($cssVal);
-    });
+    // $('.g-title').click(function() {
+    //   $cssVal = {
+    //     'display':'flex',
+    //     'flex-wrap':'wrap'
+    //   }
+    //   $(this).next('.g-main').slideToggle('fast').css($cssVal);
+    // });
     $('label').click(function() {
       $(this).parent().find('label').removeClass('active');
       $(this).addClass('active');
