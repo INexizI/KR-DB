2e019b98d689fe2b08d942d73f002aef71287a46
INexizI <39536903+INexizI@users.noreply.github.com>
2021-11-01 01:31:10 +0300

    add PWA first step

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,30 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import { Turbo } from "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
-require("channels")
+import "channels"
+import "controllers"
+
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    console.log('ServiceWorker registered: ', registration);
+
+    var serviceWorker;
+    if (registration.installing) {
+      serviceWorker = registration.installing;
+      console.log('Service worker installing.');
+    } else if (registration.waiting) {
+      serviceWorker = registration.waiting;
+      console.log('Service worker installed & waiting.');
+    } else if (registration.active) {
+      serviceWorker = registration.active;
+      console.log('Service worker active.');
+    }
+  }).catch(registrationError => {
+    console.log('Service worker registration failed: ', registrationError);
+  });
+});
+
 // require("jquery-ui")
 require("packs/char")
 require("packs/gear")
@@ -14,6 +37,7 @@ require("packs/jquery.easy-autocomplete.js")
 // import 'src/stylesheets/application'
 // const images = require.context('../images', true)
 
+window.Turbo = Turbo
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
