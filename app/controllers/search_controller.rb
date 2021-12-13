6a45fb15a5ad6bada88d245c580f2b26a4b5fd5c
INexizI <39536903+INexizI@users.noreply.github.com>
2021-12-14 02:04:03 +0300

    add limit search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,28 @@
 class SearchController < ApplicationController
+  # before_action :force_json, only: :search
+
   def index
   end
 
   def search
-    @chars = Char.order('name ASC').ransack(name_cont: params[:q]).result(distinct: true)
-    @skills = Skill.order('name ASC').ransack(name_or_description_cont: params[:q]).result(distinct: true)
-    @perks = Perk.order('name ASC').ransack(name_or_description_cont: params[:q]).result(distinct: true)
-    @gears = Gear.order('name ASC').where.not(id: [963..983]).ransack(name_or_description_cont: params[:q]).result(distinct: true)
+    @chars = Char.order('name ASC').ransack(name_cont: params[:q]).result(distinct: true).limit(10)
+    @skills = Skill.order('name ASC').ransack(name_or_description_cont: params[:q]).result(distinct: true).limit(10)
+    @perks = Perk.order('name ASC').ransack(name_or_description_cont: params[:q]).result(distinct: true).limit(10)
+    @gears = Gear.order('name ASC').where.not(id: [963..983]).ransack(name_or_description_cont: params[:q]).result(distinct: true).limit(10)
+
+    # @chars = Char.where("name ILIKE ?", "%#{params[:q]}%").limit(10)
+    # @skills = Skill.where("name ILIKE ? or description ILIKE ?", "%#{params[:q]}%", "%#{params[:q]}%").limit(10)
+    # @perks = Perk.where("name ILIKE ? or description ILIKE ?", "%#{params[:q]}%", "%#{params[:q]}%").limit(10)
+    # @gears = Gear.where("name ILIKE ? or description ILIKE ?", "%#{params[:q]}%", "%#{params[:q]}%").limit(10)
 
     respond_to do |format|
       format.html {}
       format.json {}
     end
   end
+
+  # private
+  #   def force_json
+  #     request.format = :json
+  #   end
 end
