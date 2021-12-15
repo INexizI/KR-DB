a735e854aaef12f8f537fe598dc1a1cbc5a228d0
INexizI <39536903+INexizI@users.noreply.github.com>
2021-12-16 02:10:24 +0300

    fix skill page description

--- app/javascript/packs/skill.js
+++ app/javascript/packs/skill.js
@@ -2,11 +2,16 @@
   $(document).on("turbolinks:load", function() {
     $('.s-image').mouseenter(function() {
       $left = 0;
+      $top = 0;
       if ($(document).width() - $(this).offset().left < $(document).width() / 2)
-      $left = -1;
-      $x = $left * 430;
+        $left = -1;
+      if ($(document).height() - $(this).offset().top < $(document).height() / 2)
+        $top = -1;
+      $x = $left * 308;
+      $y = $top * ($(this).find('.s-desc').height() + 60)
       $cs = {
         'left': $x,
+        'top': $y,
         'display': 'grid'
       }
       $('.s-desc', this).css($cs);
