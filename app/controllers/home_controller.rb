d897f7455c47e7c3c29c24d8ffe5c75b03d6d1c0
INexizI <yavseh@hotmail.com>
2025-02-06 00:35:25 +0300

    upd home controller + view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @chars = Char.order('RANDOM()').limit(3).load_async
-    @gears = Gear.where.not('gear_type = ?', 'acc').order('RANDOM()').limit(3).load_async
-    @perks = Perk.order('RANDOM()').limit(3).load_async
-    @runes = Rune.order('RANDOM()').limit(3).load_async
-    @skills = Skill.where('parent_id = ?', '').order('RANDOM()').limit(3).load_async
+    @chars = Char.order("RANDOM()").limit(3).load_async
+    @gears = Gear.where(gear_type: "art").order("RANDOM()").limit(3).load_async
+    @perks = Perk.where(perk_type: "hero").order("RANDOM()").limit(3).load_async
+    @runes = Rune.order("RANDOM()").limit(3).load_async
+    @skills = Skill.where(parent_id: "").order("RANDOM()").limit(3).load_async
   end
 
   def offline
